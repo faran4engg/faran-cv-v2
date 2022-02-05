af9e7c8444f80b81ba314cf1bfeeecfe5a514ab4
faran4engg <faran4engg@gmail.com>
2022-02-06 00:16:02 +0100

    🎨 feat: added tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "theme-color": "#818cf8",
+        "theme-color-hover": "#a5b4fc",
+
+        "dark-bg-1": "#252830",
+        "dark-bg-2": "#141416",
+
+        "dark-mode-text": "#eff8fe",
+      },
+    },
   },
   plugins: [],
 };
