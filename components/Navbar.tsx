f24e83147cd254aea9e6ac1ddc208f3da7abced4
faran4engg <faran4engg@gmail.com>
2022-02-06 01:13:14 +0100

    ✅ feat: added svg icons for nav bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Profile from "../assets/icons/profile.svg";
 
 const Navbar = () => {
   const arr = [
@@ -30,39 +32,25 @@ const Navbar = () => {
   return (
     <div className="absolute flex justify-center w-full mx-auto bottom-4">
       <div className="px-2 shadow-lg bg-dark-bg-1 rounded-2xl">
-        <div className="flex">
-          <div className="flex-1 group">
-            <a className="flex items-end justify-center w-full px-2 pt-2 mx-auto text-center hover:text-theme-color">
-              <span className="block px-1 pt-1 pb-1">
-                <span className="block pb-1 ">Home</span>
-                <span className="block w-5 h-1 mx-auto rounded-full group-hover:bg-theme-color-hover"></span>
-              </span>
-            </a>
-          </div>
-          <div className="flex-1 group">
-            <a className="flex items-end justify-center w-full px-2 pt-2 mx-auto text-center group-hover:text-theme-color">
-              <span className="block px-1 pt-1 pb-1">
-                <span className="block pb-1 ">Explore</span>
-                <span className="block w-5 h-1 mx-auto rounded-full group-hover:bg-theme-color-hover"></span>
-              </span>
-            </a>
-          </div>
-          <div className="flex-1 group">
-            <a className="flex items-end justify-center w-full px-2 pt-2 mx-auto text-center group-hover:text-theme-color">
-              <span className="block px-1 pt-1 pb-1">
-                <span className="block pb-1 ">Search</span>
-                <span className="block w-5 h-1 mx-auto rounded-full group-hover:bg-theme-color-hover"></span>
-              </span>
-            </a>
-          </div>
-          <div className="flex-1 group">
-            <a className="flex items-end justify-center w-full px-2 pt-2 mx-auto text-center group-hover:text-theme-color">
-              <span className="block px-1 pt-1 pb-1">
-                <span className="block pb-1 ">Settings</span>
-                <span className="block w-5 h-1 mx-auto rounded-full group-hover:bg-theme-color-hover"></span>
-              </span>
-            </a>
-          </div>
+        <div className="flex gap-0">
+          {[1, 2, 3, 4].map((item) => (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              whileHover={{ scale: 2, y: -10 }}
+              whileTap={{ scale: 2, y: -10 }}
+              key={item}
+              className="flex-1 group"
+            >
+              <a className="flex items-end justify-center w-full px-2 pt-2 mx-auto text-center hover:text-theme-color">
+                <span className="block px-1 pt-1 pb-1">
+                  <Image src={Profile} height={30} width={30} alt="test" />
+                  {/* <span className="block pb-1 "> {item}</span> */}
+                  {/* <span className="block w-5 h-1 mx-auto rounded-full group-hover:bg-theme-color-hover"></span> */}
+                </span>
+              </a>
+            </motion.div>
+          ))}
         </div>
       </div>
     </div>
